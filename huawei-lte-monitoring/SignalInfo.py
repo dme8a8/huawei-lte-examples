17359b94084ce9c68ce73ef9606a5a17dd5e4f5a
dme8a8 <47527153+dme8a8@users.noreply.github.com>
2019-12-21 16:05:23 +0100

    Update SignalInfo.py
    UMTS support added

--- huawei-lte-monitoring/SignalInfo.py
+++ huawei-lte-monitoring/SignalInfo.py
@@ -1,5 +1,8 @@
 def get_int(value):
-    return int(value.split('d')[0])
+	if value == None:
+	  return int(0)
+	else:
+	  return int(value.split('d')[0])
 
 class SignalInfo():
     def __init__(self, client):
@@ -13,3 +16,12 @@ class SignalInfo():
 
     def get_sinr(self):
         return get_int(self.signal_info['sinr'])
+
+    def get_rssi(self):
+        return get_int(self.signal_info['rssi'])
+        
+    def get_ecio(self):
+        return get_int(self.signal_info['ecio'])
+        
+    def get_rscp(self):
+        return get_int(self.signal_info['rscp'])
